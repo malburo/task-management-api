4d09aeeb72fd47c400acc4c1e0850e61befbc69a
vunguyengitacc <nguyenhoanganhvu.it@gmail.com>
2021-10-01 17:00:13 +0700

    add search channel api

--- src/modules/Room/room.controller.js
+++ src/modules/Room/room.controller.js
@@ -3,6 +3,28 @@ import Room from './room.model';
 import roomService from './room.service';
 import User from '../User/user.model';
 
+const getAllYourChannel = async (req, res, next) => {
+  try {
+    const generalRooms = await Room.find({ isGeneral: true, userId: req.user._id }).populate('board').lean();
+    const channels = generalRooms.map((i) => i.board);
+    Result.success(res, { channels });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const searchChannel = async (req, res, next) => {
+  try {
+    const { term } = req.query;
+    const generalRooms = await Room.find({ isGeneral: true, userId: req.user._id }).populate('board').lean();
+    const allChannels = generalRooms.map((i) => i.board);
+    const channels = allChannels.filter((i) => i.title.match(new RegExp(term, 'i')));
+    Result.success(res, { channels });
+  } catch (err) {
+    next(err);
+  }
+};
+
 const getAllYourRoomInBoard = async (req, res, next) => {
   try {
     const boardId = req.params;
@@ -10,7 +32,11 @@ const getAllYourRoomInBoard = async (req, res, next) => {
     rooms = rooms.filter((r) => r.userId.some((i) => i.toString() == req.user._id));
     rooms = rooms.map((i) => {
       if (i.isGeneral) i.name = i.board.title;
-      else i.name = i.members.filter((m) => m._id.toString() != req.user._id.toString())[0].fullname;
+      else {
+        let data = i.members.filter((m) => m._id.toString() != req.user._id.toString())[0];
+        i.name = data.fullname;
+        i.image = data.profilePictureUrl;
+      }
       return i;
     });
     Result.success(res, { rooms });
@@ -35,6 +61,7 @@ const create = async (req, res, next) => {
   try {
     const data = { ...req.body };
     data.userId = [req.user._id];
+    data.isGeneral = true;
     const newRoom = await roomService.create(data);
     Result.success(res, { newRoom });
   } catch (err) {
@@ -99,11 +126,13 @@ const removeMember = async (req, res, next) => {
 };
 
 const roomController = {
+  getAllYourChannel,
   getAllYourRoomInBoard,
   getOne,
   create,
   addMember,
   removeMember,
+  searchChannel,
 };
 
 export default roomController;
--- src/modules/Room/room.route.js
+++ src/modules/Room/room.route.js
@@ -2,7 +2,8 @@ import express from 'express';
 import roomController from './room.controller';
 const RoomRouter = express.Router();
 
-RoomRouter.route('/channel').post(roomController.create);
+RoomRouter.route('/channel').get(roomController.getAllYourChannel).post(roomController.create);
+RoomRouter.route('/channel/search').get(roomController.searchChannel);
 RoomRouter.route('/board/:boardId/member').post(roomController.addMember).delete(roomController.removeMember);
 RoomRouter.route('/board/:boardId').get(roomController.getAllYourRoomInBoard);
 RoomRouter.route('/:roomId').get(roomController.getOne);
