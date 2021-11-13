87e733677908a1a4ee9c48a9d2482f13a93e64fc
malburo <tongquocbao0605@gmail.com>
2021-11-14 00:01:15 +0700

    update notification module

--- src/modules/Notification/notification.model.js
+++ src/modules/Notification/notification.model.js
@@ -2,13 +2,12 @@ import { model, Schema } from 'mongoose';
 
 const NotificationSchema = new Schema(
   {
-    notificationType: String,
     content: String,
     isRead: Boolean,
-    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+    notificationType: String,
     boardId: { type: Schema.Types.ObjectId, ref: 'boards' },
-    receiversId: [{ type: Schema.Types.ObjectId, ref: 'users' }],
-    taskOrder: [{ type: Schema.Types.ObjectId, ref: 'tasks' }],
+    senderId: { type: Schema.Types.ObjectId, ref: 'users' },
+    receiverId: { type: Schema.Types.ObjectId, ref: 'users' },
   },
   {
     toJSON: {
