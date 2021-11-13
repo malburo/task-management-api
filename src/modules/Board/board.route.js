da1da17abc1a7f51d15ae7caa4af0c9224963d04
malburo <tongquocbao0605@gmail.com>
2021-11-14 00:00:45 +0700

    update board module

--- src/modules/Board/board.route.js
+++ src/modules/Board/board.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import historyController from 'modules/Activity/activity.controller';
 import boardController from './board.controller';
 const BoardRouter = express.Router();
 
 BoardRouter.route('/').get(boardController.getAll).post(boardController.create);
 BoardRouter.route('/:boardId').get(boardController.getOne).put(boardController.update);
+BoardRouter.route('/:boardId/activities').get(historyController.getActivityByBoardId);
 
 export default BoardRouter;
