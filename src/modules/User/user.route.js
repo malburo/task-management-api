8e9b0decf2a9c1e9147fa3f504db76b994ce3a89
malburo <tongquocbao0605@gmail.com>
2021-11-15 19:31:43 +0700

    feature/activity and push notification

--- src/modules/User/user.route.js
+++ src/modules/User/user.route.js
@@ -5,5 +5,4 @@ const UserRouter = Router();
 UserRouter.route('/').get(userController.getAll);
 UserRouter.route('/:userId').put(userController.updateInfo).delete(userController.deleteUser);
 UserRouter.route('/:userId/change-password').put(userController.updatePassword);
-
 export default UserRouter;
